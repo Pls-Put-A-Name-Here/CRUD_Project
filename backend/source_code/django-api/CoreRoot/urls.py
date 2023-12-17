b0207ec22db6055838ea8be669c94a62446ab5de
YOUNG KIRK <adjanour@icloud.com>
2023-12-17 21:48:36 +0000

    modified directory structure -- breaking changes

--- backend/source_code/django-api/CoreRoot/urls.py
+++ backend/source_code/django-api/CoreRoot/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from core.routers import router
+from django.db import router
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
